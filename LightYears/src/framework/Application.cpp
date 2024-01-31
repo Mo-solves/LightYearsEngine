1e6f675647b221d954cf17b57f5720072e911510
Mohamed Abdi <mosolves147@gmail.com>
2024-01-31 09:39:49 +0300

    Set up the game loop

--- LightYears/src/framework/Application.cpp
+++ LightYears/src/framework/Application.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
 #include "framework/Application.h"
 
 namespace ly
 {
 	Application::Application()
-		:mWindow{sf::VideoMode(900,900), "Light Years"}
+		:mWindow{sf::VideoMode(900,900), "Light Years"},
+		mTargerFrameRate{30.f},
+		mTickClock{}
 	{
 		
 	}
 	void Application::Run()
 	{
+		mTickClock.restart();
+		float accumulatedTime = 0.f;
+		float targetDeltaTime = 1.f / mTargerFrameRate;
 		while (mWindow.isOpen())
 		{
 			sf::Event windowEvent;
@@ -19,6 +25,24 @@ namespace ly
 					mWindow.close();
 				}
 			}
+
+			float frameDeltaTime = mTickClock.restart().asSeconds();
+			accumulatedTime += frameDeltaTime;
+
+			while (accumulatedTime > targetDeltaTime)
+			{
+				accumulatedTime -= targetDeltaTime;
+				Tick(targetDeltaTime);
+				Render();
+			}
+			std::cout << "ticking at framerate: " << 1.f / frameDeltaTime << '\n';
+
 		}
+	}
+	void Application::Tick(float deltaTime)
+	{
+	}
+	void Application::Render()
+	{
 	}
 }
