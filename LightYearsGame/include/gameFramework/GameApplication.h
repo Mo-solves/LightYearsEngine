6122bf61b4fb650525aea2575dc9c184c5a71334
Mohamed Abdi <mosolves147@gmail.com>
2024-02-06 18:03:23 +0300

    create spaceship and playerSpaceship classes

--- LightYearsGame/include/gameFramework/GameApplication.h
+++ LightYearsGame/include/gameFramework/GameApplication.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <framework/Application.h>
 #include <framework/Actor.h>
+#include "spaceship/Spaceship.h"
 
 namespace ly
 {
 	class Actor;
+	class Spaceship;
 	class GameApplication : public Application
 	{
 	public:
@@ -12,6 +14,6 @@ namespace ly
 		virtual void Tick(float deltaTime) override;
 	private:
 		float counter;
-		weak<Actor> actorToDestroy;
+		weak<Spaceship> testPlayerSpaceship;
 	};
 }
