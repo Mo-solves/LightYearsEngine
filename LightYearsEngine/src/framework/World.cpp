68fd78ff8b52ed682fc5b8623ecf6b62c123333b
Mohamed Abdi <mosolves147@gmail.com>
2024-02-17 11:47:32 +0300

    Create the Wait Stage and refine stage cleanup Control

--- LightYearsEngine/src/framework/World.cpp
+++ LightYearsEngine/src/framework/World.cpp
@@ -12,7 +12,7 @@ namespace ly
 		mBeganPlay(false),
 		mActors{},
 		mPendingActors{},
-		mCurrentStageIndex{-1},
+		mCurrentStage{mGameStages.end()},
 		mGameStages{}
 	{
 
@@ -25,7 +25,7 @@ namespace ly
 			mBeganPlay = true;
 			BeginPlay();
 			InitGameStages();
-			NextGameStage();
+			StartStages();
 		}
 	}
 
@@ -45,9 +45,9 @@ namespace ly
 			++iter;
 		}
 
-		if (mCurrentStageIndex >= 0 && mCurrentStageIndex < mGameStages.size())
+		if (mCurrentStage != mGameStages.end())
 		{
-			mGameStages[mCurrentStageIndex]->TickStage(deltaTime);
+			mCurrentStage->get()->TickStage(deltaTime);
 		}
 
 		Tick(deltaTime);
@@ -85,18 +85,6 @@ namespace ly
 				++iter;
 			}
 		}
-
-		for (auto iter = mGameStages.begin(); iter != mGameStages.end();)
-		{
-			if (iter->get()->IsStageFinished())
-			{
-				iter = mGameStages.erase(iter);
-			}
-			else
-			{
-				++iter;
-			}
-		}
 	}
 
 	void World::AddStage(const shared<GameStage>& newStage)
@@ -121,22 +109,28 @@ namespace ly
 
 	void World::AllGameStageFinished()
 	{
-
+		LOG("All Stage Finished");
 	}
 
 	void World::NextGameStage()
 	{
-		++mCurrentStageIndex;
+		mCurrentStage = mGameStages.erase(mCurrentStage);
 
-		if (mCurrentStageIndex >= 0 && mCurrentStageIndex < mGameStages.size())
+		if (mCurrentStage != mGameStages.end())
 		{
-			mGameStages[mCurrentStageIndex]->onStageFinished.BindAction(
-				GetWeakRef(), &World::NextGameStage);
-			mGameStages[mCurrentStageIndex]->StartStage();
+			mCurrentStage->get()->StartStage();
+			mCurrentStage->get()->onStageFinished.BindAction(GetWeakRef(), &World::NextGameStage);
 		}
 		else
 		{
 			AllGameStageFinished();
 		}
+	}
+
+	void World::StartStages()
+	{
+		mCurrentStage = mGameStages.begin();
+		mCurrentStage->get()->StartStage();
+		mCurrentStage->get()->onStageFinished.BindAction(GetWeakRef(), &World::NextGameStage);
 	}
 }
---
+++ LightYearsEngine/src/gameplay/WaitStage.cpp
@@ -0,0 +1,16 @@
+#include "framework/TimerManager.h"
+#include "gameplay/WaitStage.h"
+
+namespace ly
+{
+	WaitStage::WaitStage(World* world, float waitDuration)
+		:GameStage{world},
+		mWaitDuration{waitDuration}
+	{
+	}
+
+	void WaitStage::StartStage()
+	{
+		TimerManager::Get().SetTimer(GetWeakRef(), &WaitStage::FinishStage, mWaitDuration);
+	}
+}
